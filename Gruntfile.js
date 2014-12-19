ea8c8c8c4306edb9265cfa1600766bd61d4c3873
Vadim Makeev <pepelsbey@gmail.com>
2014-12-19 21:53:09 +0100

    Now with build task

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		compress: {
+			zip: {
+				options: {
+					archive: '<%= pkg.name %>.nex',
+					mode: 'zip'
+				},
+				src: [
+					'_locales/*',
+					'images/*',
+					'background.js',
+					'manifest.json'
+				]
+			}
+		}
+
+	});
+
+	grunt.registerTask('default', 'compress');
+
+};
