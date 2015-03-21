a5a218e63457d20a2a0acb373c96d6e89b880608
pepelsbey <pepelsbey@gmail.com>
2015-03-21 01:06:39 +0100

    New build tasks & dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,26 @@ module.exports = function(grunt) {
 
 		pkg: grunt.file.readJSON('package.json'),
 
+		imagemin: {
+			task: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: '*.{png,jpg}',
+					dest: 'images/'
+				},{
+					expand: true,
+					cwd: 'pictures/',
+					src: '*.{png,jpg}',
+					dest: 'pictures/'
+				}]
+			}
+		},
+
 		compress: {
 			zip: {
 				options: {
-					archive: '<%= pkg.name %>.nex',
-					mode: 'zip'
+					archive: '<%= pkg.name %>.zip'
 				},
 				src: [
 					'_locales/**',
@@ -19,10 +34,21 @@ module.exports = function(grunt) {
 					'manifest.json'
 				]
 			}
+		},
+
+		copy: {
+			nex: {
+				src: '<%= pkg.name %>.zip',
+				dest: '<%= pkg.name %>.nex'
+			}
 		}
 
 	});
 
-	grunt.registerTask('default', 'compress');
+	grunt.registerTask('default', [
+		'imagemin',
+		'compress',
+		'copy'
+	]);
 
 };
