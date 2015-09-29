c7bb6b4979bbd25914595a62ea0d4f2f0ee8f23e
Vadim Makeev <pepelsbey@gmail.com>
2015-09-29 18:32:41 +0200

    Code style

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.update(tab.id, {
-    url: 'things:'
-  }, function() {
-    setTimeout(function() {
-      chrome.tabs.update(tab.id, {
-        url: 'things:add?title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url)
-      });
-    }, 300);
-  });
+	chrome.tabs.update(tab.id, {
+		url: 'things:'
+	}, function() {
+		setTimeout(function() {
+			chrome.tabs.update(tab.id, {
+				url: 'things:add?title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url)
+			});
+		}, 300);
+	});
 });
