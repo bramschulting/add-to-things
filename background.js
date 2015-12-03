662bf78542976412819b4f6688f2e9dd09a5d413
shajiquan <shajiquan@gmail.com>
2015-12-03 18:17:01 +0800

    Add new feature: append selected text to note

--- background.js
+++ background.js
@@ -1,11 +1,20 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.update(tab.id, {
-		url: 'things:'
-	}, function() {
-		setTimeout(function() {
-			chrome.tabs.update(tab.id, {
-				url: 'things:add?title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url)
-			});
-		}, 300);
-	});
+    chrome.tabs.executeScript(tab.id, {
+        // run script to try to get user selected text
+        code: "window.getSelection().toString()"
+    },function(selection) {
+        // format selection if it is not null
+        selection = selection[0] ? "\n" + selection[0]: "";
+
+        chrome.tabs.update(tab.id, {
+            url: 'things:'
+        },function() {
+            setTimeout(function() {
+                chrome.tabs.update(tab.id, {
+                    url: 'things:add?title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url + selection)
+                });
+            },300);
+        });
+    });
+
 });
