f3304fa2dcc6c1f883601a4c2d90853c2e915c6e
Vadim Makeev <pepelsbey@gmail.com>
2015-12-04 01:05:26 +0100

    Code style

--- background.js
+++ background.js
@@ -1,20 +1,21 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {
-        // run script to try to get user selected text
-        code: "window.getSelection().toString()"
-    },function(selection) {
-        // format selection if it is not null
-        selection = selection[0] ? "\n" + selection[0]: "";
+        // Run script to try to get user selected text
+        code: 'window.getSelection().toString()'
+    }, function(selection) {
+        // Format selection if it’s not null
+        selection = selection[0] ? '\n\n' + selection[0] : '';
 
         chrome.tabs.update(tab.id, {
             url: 'things:'
-        },function() {
+        }, function() {
             setTimeout(function() {
                 chrome.tabs.update(tab.id, {
-                    url: 'things:add?title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url + selection)
+                    url:
+                        'things:add?title=' + encodeURIComponent(tab.title) +
+                        '&notes=' + encodeURIComponent(tab.url + selection)
                 });
-            },300);
+            }, 300);
         });
     });
-
 });
