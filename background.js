59f76190e92cc1855b22fde143b9f725cde4d890
Vadim Makeev <pepelsbey@gmail.com>
2016-04-26 11:18:42 +0300

    Selection doesn’t work well

--- background.js
+++ background.js
@@ -1,21 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, {
-        // Run script to try to get user selected text
-        code: 'window.getSelection().toString()'
-    }, function(selection) {
-        // Format selection if it’s not null
-        selection = selection[0] ? '\n\n' + selection[0] : '';
-
-        chrome.tabs.update(tab.id, {
-            url: 'things:'
-        }, function() {
-            setTimeout(function() {
-                chrome.tabs.update(tab.id, {
-                    url:
-                        'things:add?title=' + encodeURIComponent(tab.title) +
-                        '&notes=' + encodeURIComponent(tab.url + selection)
-                });
-            }, 300);
-        });
-    });
+	chrome.tabs.update(tab.id, {
+		url: 'things:'
+	}, function() {
+		setTimeout(function() {
+			chrome.tabs.update(tab.id, {
+				url: 'things:add?title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url)
+			});
+		}, 300);
+	});
 });
