dbcc8e2fcadd45cc719bec27d5c1b5d83029d1b5
Vadim Makeev <pepelsbey@gmail.com>
2018-06-23 23:50:53 +0300

    Fix indentation

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ const zip = require('gulp-zip');
 
 gulp.task('default', () => {
 	return gulp.src([
-			'_locales/**',
-			'images/**',
-			'background.js',
-			'manifest.json'
-		], {
-			base: '.'
-		})
-		.pipe(zip('add-to-things.zip'))
-		.pipe(gulp.dest('.'))
-		.pipe(zip('add-to-things.nex'))
-		.pipe(gulp.dest('.'));
+		'_locales/**',
+		'images/**',
+		'background.js',
+		'manifest.json'
+	], {
+		base: '.'
+	})
+	.pipe(zip('add-to-things.zip'))
+	.pipe(gulp.dest('.'))
+	.pipe(zip('add-to-things.nex'))
+	.pipe(gulp.dest('.'));
 });
