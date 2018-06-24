f0894ebb6db5e0ef4ebe2e20afda7f209f5bac36
Vadim Makeev <pepelsbey@gmail.com>
2018-06-24 17:04:47 +0300

    Replace tabs with spaces

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 
-	chrome.tabs.executeScript(tab.id, {
-		// run script to try to get user selected text
-		code: "window.getSelection().toString()"
-	},function(selection) {
-		// format selection if it is not null
-		selection = selection ? "\n" + selection[0]: "";
+    chrome.tabs.executeScript(tab.id, {
+        // run script to try to get user selected text
+        code: 'window.getSelection().toString()'
+    },function(selection) {
+        // format selection if it is not null
+        selection = selection ? '\n' + selection[0]: '';
 
-		// Things 3.4 URL Scheme
-		// https://support.culturedcode.com/customer/en/portal/articles/2803573
-		var thingsURL = 'things:///add?show-quick-entry=true&title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url + selection);
-  		chrome.tabs.update({ url: thingsURL });
-	});
+        // Things 3.4 URL Scheme
+        // https://support.culturedcode.com/customer/en/portal/articles/2803573
+        var thingsURL = 'things:///add?show-quick-entry=true&title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url + selection);
+          chrome.tabs.update({ url: thingsURL });
+    });
 
 });
