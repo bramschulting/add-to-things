f7e1dd53e5a04b1eaa0dabe575229f62da8f6d02
bleleux <leleuxart@gmail.com>
2022-07-23 13:12:46 -0700

    Changes for manifest v3

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
 
-    chrome.tabs.executeScript(tab.id, {
-        // run script to try to get user selected text
-        code: 'window.getSelection().toString()'
-    },function(selection) {
-        // format selection if it is not null
-        selection = selection ? '\n' + selection[0]: '';
+chrome.action.onClicked.addListener((tab) => {
 
-        // Things 3.4 URL Scheme
-        // https://support.culturedcode.com/customer/en/portal/articles/2803573
-        var thingsURL = 'things:///add?show-quick-entry=true&title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url + selection);
-          chrome.tabs.update({ url: thingsURL });
-    });
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    // run script to try to get user selected text
+    function: () => window.getSelection().toString()
+  }, function (selection) {
+    // format selection if it is not null
+    selection = selection[0].result ? '\n' + selection[0].result: '';
 
+    // Things 3.4 URL Scheme
+    // https://support.culturedcode.com/customer/en/portal/articles/2803573
+    var thingsURL = 'things:///add?show-quick-entry=true&title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(tab.url + selection);
+    chrome.tabs.update({ url: thingsURL });
+
+  });
+  
 });
